ab92a95ee1271be7544c733e709a91a797c77bc4
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-04-30 14:26:26 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(names, arg) {
+  return names.filter((mathes) => mathes.toLowerCase() === arg.toLowerCase());
+}
+
+function fuzzyMatch(names, arg) {
+  return names.filter(
+    (mathes) => mathes.toLowerCase().indexOf(arg.toLowerCase()) === 0
+  );
+}
+
+function matchName(names, arg) {
+  return names.filter((record) => record.name === arg);
+}
